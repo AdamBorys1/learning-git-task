67e0ada178f13ccb7ac43ff5903ee236d8c0f575
Adam Borys <adamborys2002@wp.pl>
2021-09-18 13:14:29 +0200

    specjalne pozdrowienia

--- zakupy.py
+++ zakupy.py
@@ -1,14 +1,1 @@
-shopping = {
-    "piekarnia": ['chleb', 'bułka', 'pączek'],
-    "warzywniak": ['cebula', 'buraki', 'seler']
-}
-
-for shop, product in shopping.items():
-    product = [x.title() for x in product]
-    print(f"Idę do {shop.capitalize()} i kupuję tam {product}")
-
-number = int(len(shopping.get("piekarnia")) + int(len(shopping.get("warzywniak"))))
-print(f"W sumie kupuję {number} produktów")
-
-print("Tworzę drugi commit do wysłania")
-print("Drugi commit został dodany, lecimy z trzecim :)")
+# Specjalne pozdrowienia dla mojego Mentora!
