13e1940d3877045fc03d83e042b50ecf331d5dcf
Adam Borys <adamborys2002@wp.pl>
2021-09-18 13:11:33 +0200

    Zmiana2

--- zakupy.py
+++ zakupy.py
@@ -11,3 +11,4 @@ number = int(len(shopping.get("piekarnia")) + int(len(shopping.get("warzywniak")
 print(f"W sumie kupuję {number} produktów")
 
 print("Tworzę drugi commit do wysłania")
+print("Drugi commit został dodany, lecimy z trzecim :)")
