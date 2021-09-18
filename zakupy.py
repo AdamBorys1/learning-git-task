3c4c0e94a9b5b3d35ae7e3c8d8d100352f61da1c
Adam Borys <adamborys2002@wp.pl>
2021-09-18 13:09:33 +0200

    Zmiana1

--- zakupy.py
+++ zakupy.py
@@ -6,3 +6,8 @@ shopping = {
 for shop, product in shopping.items():
     product = [x.title() for x in product]
     print(f"Idę do {shop.capitalize()} i kupuję tam {product}")
+
+number = int(len(shopping.get("piekarnia")) + int(len(shopping.get("warzywniak"))))
+print(f"W sumie kupuję {number} produktów")
+
+print("Tworzę drugi commit do wysłania")
